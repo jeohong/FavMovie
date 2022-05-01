67fd0fccf3910cfde6fbbb1e0f52e5c54f6c8588
jeohong <jeohong59@gmail.com>
2022-05-01 11:16:39 +0900

    Initial Commit

---
+++ FavMovie/FavMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavMovieApp.swift
+//  FavMovie
+//
+//  Created by Hong jeongmin on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct FavMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
